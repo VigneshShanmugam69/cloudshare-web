264fed529341388285227ad34d8e5367e7c08e21
mahalakshmimonisha123 <118173387+mahalakshmimonisha123@users.noreply.github.com>
2023-04-28 18:44:53 +0530

    Move to completed

--- CloudShare/src/app/dashboard/dashboard.component.ts
+++ CloudShare/src/app/dashboard/dashboard.component.ts
@@ -354,12 +354,19 @@ export class DashboardComponent {
 
   contextMenuPosition = { x: '0px', y: '0px' };
   onContextMenu(event: MouseEvent, row: any) {
-    
+
     event.preventDefault();
     this.contextMenuPosition.x = event.clientX + 'px';
     this.contextMenuPosition.y = event.clientY + 'px';
     this.contextMenu.openMenu();
+    
   }
+
+  // menuClose() {
+  //   this.contextMenu!.closeMenu(); 
+  // }
+
+
   onRowClick(row: any) {
     this.rowValue = row;
     console.log("rowvalue", this.rowValue)
@@ -367,51 +374,58 @@ export class DashboardComponent {
   }
   // open the popup both right use (contextmenu) and left click (click)
 
-  
 
 
-  copyto() {
+
+  copyto(action: string) {
     // event.preventDefault();
     this.tableObjects = this.rowValue;
-    console.log("tableObjects Key: ", this.tableObjects.Key);
+    this.contextMenu.closeMenu(); 
     const dialogRef = this.dialog.open(ObjectpopupComponent, {
 
       width: '414px',
       height: '350px',
-      
+
 
       data: {
-      bucket: this.bucketname,
-      object: this.tableObjects
-    }
+        action: action,
+        bucket: this.bucketname,
+        object: this.tableObjects
+      }
+
     });
     dialogRef.afterClosed().subscribe(result => {
 
     });
-}
+  }
 
 
 
 
 
-@HostListener('document:keydown.control.shift.c', ['$event'])
-onCustomKeyDown(event: KeyboardEvent, row: any) {
-  this.tableObjects = this.rowValue;
 
-  console.log("keyboard", this.tableObjects)
-  const dialog = this.dialog.open(ObjectpopupComponent, {
-    width: '414px',
-    height: '350px',
-    data: {
-      bucket: this.bucketname,
-      object: this.tableObjects
-    }
-  });
-  dialog.afterClosed().subscribe(result => {
 
-  });
+  @HostListener('document:keydown.control.shift.c', ['$event'])
+  @HostListener('document:keydown.control.shift.x', ['$event'])
+  onCustomKeyDown(event: KeyboardEvent, row: any) {
+    this.tableObjects = this.rowValue;
 
-}
+    console.log("keyboard", this.tableObjects)
+    const dialog = this.dialog.open(ObjectpopupComponent, {
+      width: '414px',
+      height: '350px',
+      data: {
+        bucket: this.bucketname,
+        object: this.tableObjects,
+      }
+
+    });
+
+    dialog.afterClosed().subscribe(result => {
+
+    });
+
+  }
 }
 
 
