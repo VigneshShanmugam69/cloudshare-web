a631af09cf339c39e0a525c7c0471b0cb128afef
VigneshShanmugam69 <vigneshshanmugam69@gmail.com>
2023-05-05 17:45:37 +0530

    configuration component design

--- CloudShare/src/app/admindashboard/configuration/configuration.component.ts
+++ CloudShare/src/app/admindashboard/configuration/configuration.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-configuration',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./configuration.component.css']
 })
 export class ConfigurationComponent {
+
+  configureurl = new FormGroup({
+    acsurl: new FormControl(""),
+    issuer: new FormControl(""),
+    metadata: new FormControl(""),
+    embed: new FormControl("")
+  })
+
+
+  saveconfig(){
+    let payload={
+      acs:this.configureurl.controls.acsurl.value,
+      issuer:this.configureurl.controls.issuer.value,
+      metadata:this.configureurl.controls.metadata.value,
+      embed:this.configureurl.controls.embed.value,
+    }
+    console.log(payload)
+  }
 
 }
